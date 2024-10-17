dbe19979f1f62d32692f8e0b0c3fe4887f56a5fb
sonalidhage2005 <156876212+sonalidhage2005@users.noreply.github.com>
2024-10-17 11:29:05 +0530

    Code for Experiment 1

---
+++ Experiment-1.cpp
@@ -0,0 +1,37 @@
+//Sonali Dhage
+//PRN 23070123128
+//Exp 1.1
+#include<iostream>
+
+int main(){
+std::cout<<"Hello World";
+
+return 0;
+}
+
+//Sonali Dhage
+//PRN 23070123128
+//Exp 1.2
+#include<iostream>
+using namespace std;
+int main()
+
+{
+
+int num1, num2, num3, sum=0;
+
+cout<<"enter first number-";
+
+cin>>num1;
+
+cout<<"enter second number";
+
+cin>>num2;
+
+sum=num1+num2;
+
+cout<<"sum of given numbers="<<sum;
+
+return 0;
+
+}
